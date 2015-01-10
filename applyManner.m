aa46336c00e6282a8973a418d9538983481cccd1
mekline <melissa.e.kline@gmail.com>
2015-01-10 18:44:34 -0500

    finished loop manner, the bastard

--- applyManner.m
+++ applyManner.m
@@ -42,7 +42,7 @@ switch mannername
         rotations = 30*sawtooth((mult)*pi*(t-t(1))/(t(end)-t(1))+pi/2, 0.5);
         xman = x;
         yman = y; 
-    case 'sineWave' 
+    case 'sine' 
         % 3 complete sines per ~second
         rotations = zeros(lens,1);
         numSin = floor(lens/30)*3;
@@ -56,6 +56,14 @@ switch mannername
         t = 1:lens;
         xman = x - abs(transpose(sin(numSin*2*pi*t/lens)*20));
         yman = y;
+    case 'loop'
+        %Nice little loop the loop
+        %(enraging, hard to parametrize)
+        rotations = zeros(lens,1);
+        numSin = floor(lens/30);
+        t = transpose(1:lens);
+        xman = x + sin(-numSin*2*pi*(t)/(lens-1)+pi/4)*30-sin(-numSin*2*pi/(lens-1)+pi/4)*30;
+        yman = y + cos(-numSin*2*pi*(t)/(lens-1)+pi/4)*30-cos(-numSin*2*pi/(lens-1)+pi/4)*30 ;
     case 'stopstart'
         %Takes the existing points, and execute them on another timescale
         rotations = zeros(lens,1);
@@ -96,10 +104,10 @@ switch mannername
         
         xman = final_x;
         yman = final_y;
-    case 'squareWave' 
+    case 'squarewave' 
         % Thresholds sineWave so it hops back and forth between 1 and -1,
         % otherwise identical!
-        %No, actually just one cycle/sec, and cos so we start/end in right
+        %No, actually just one cycle/sec, and cosine so we start/end in right
         %place!
         rotations = zeros(lens,1);
         numSin = floor(lens/30);
@@ -113,7 +121,60 @@ switch mannername
         end
         
         yman = y;
-
+    case 'backforth'
+        %Spread out the points to include some backtracks in the path
+        %(similar to stopstart)
+        %Takes the existing points, and execute them on another timescale
+        rotations = zeros(lens,1);
+        numPause = floor(lens/30)*2;
+        final_x = [];
+        final_y = [];
+        t = 1;
+        
+        %Split up the (x,y) array into chunks
+        for j = 1:numPause
+            t2 = floor(j*lens/numPause);
+            myset = [x(t:t2) y(t:t2)];
+            new_x = [];
+            new_y = [];
+            lastpoint = myset(end, :);
+            for k=1:length(myset)
+                %For each chunk, throw out 1/3 of the points, and smooth
+                %the rest
+                if mod(k,3) ~= 0
+                    new_x(end+1) = myset(k,1);
+                    new_y(end+1) = myset(k,2);
+                end
+            end  
+            [new_x, new_y] = smoothPath(new_x,new_y);
+            %Then, grab the last space of points and use it to run
+            %backwards
+            lenLeft = (t2-t)-length(new_x);
+            halfway = floor(lenLeft/2);
+            newer_x = new_x;
+            newer_y = new_y;
+            for k=1:(lenLeft)
+                    newer_x = [newer_x; new_x(end-k)];
+                    newer_y = [newer_y; new_y(end-k)];    
+            end
+            
+            final_x = [final_x transpose(newer_x)];
+            final_y = [final_y transpose(newer_y)];
+            
+            t = t2;
+        end
+        
+        %Another one-off error, just add it to the end :(
+        final_x = [final_x lastpoint(1)];
+        final_y = [final_y lastpoint(2)];
+        
+        %And smooth the paths!
+        
+        [final_x, final_y] = smoothPath(final_x,final_y);
+        xman = final_x;
+        yman = final_y;
+    
+        
     
    
 end
