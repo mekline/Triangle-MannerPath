a5d2a6a605c1fdca5052147c09b1fc1a38805ae8
mekline <melissa.e.kline@gmail.com>
2015-06-23 13:40:12 -0400

    Attempting a new reference color!

--- Stims/drawOnBackground.m
+++ Stims/drawOnBackground.m
@@ -1,7 +1,7 @@
 function newImg = drawOnBackground(img, backimg, x, y)
 
 %This takes a (color) image matrix, and draws it on another, but skips replace
-%if the img is black at that pixel.
+%if the img is [0 255 0] at that pixel.
 %x, y gives the top x and y of where you'll draw the img
 %tolerance lets us deal with fuzzy image boundaries!
 
@@ -16,8 +16,12 @@ newImg = backimg;
 for i=1:q
     for j=1:r
         pix = img(i, j, :); %Get that pixel in the img
-        isBlack = sum(pix); %check if black (we'll allow for variance!)
-        if isBlack > tolerance
+        isBG = 0; %check if it's the stupid green (we'll allow for variance!)
+        refColor = [0 255 0];
+        for j=1:3
+        	isBG = isBG + |pix(j) - refcolor(j)| %We want to be a minimum total distance from the refcolor
+
+        if isBG > tolerance
             newImg(x+i-1, y+j-1,:) = pix; %bump the index so the first pixel is still at (100,100)!
             
         end
