fe3c587fc9a17a4be5795439d7f621492993d04f
mekline <melissa.e.kline@gmail.com>
2015-07-08 11:49:23 -0400

    fixed wrong path start points

--- Stims/drawOnBackground_black.m
+++ Stims/drawOnBackground_black.m
@@ -1,7 +1,7 @@
 function newImg = drawOnBackground(img, backimg, x, y)
 
 %This takes a (color) image matrix, and draws it on another, but skips replace
-%if the img is near [0 255 0] at that pixel.
+%if the img is [0 255 0] at that pixel.
 %x, y gives the top x and y of where you'll draw the img
 %tolerance lets us deal with fuzzy image boundaries!
 
@@ -15,15 +15,22 @@ newImg = backimg;
 
 for i=1:q
     for j=1:r
-        pix = impixel(img, j, i); %Get that pixel in the img
-        %check if it's the  GREEN (we'll allow for variance!)
-        pix
-        if pdist([pix; 0 255 0], 'Euclidean') < 50
-            newImg(x+i-1, y+j-1,:) = pix; %bump the index so the first pixel is still at correct loc in the larger pic!
+        pix = img(i, j, :); %Get that pixel in the img
+        %check if it's the  BLACK (we'll allow for variance!)
+        isBG = sum(pix);
+        %isBG = 150;
+
+        if isBG > tolerance
+            newImg(x+i-1, y+j-1,:) = pix; %bump the index so the first pixel is still at (100,100)!
             
         end
     end
 end
 
 
+end
+
+function isAbove = thresh(mypix)
+    
+    isAbove = 0;
 end
